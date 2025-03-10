93dd3c835cc6d63846fda818a3a6ad238bac0ac1
gustavo-pedro-meira <gustavomeira@ads.fiponline.edu.br>
2025-03-10 16:18:01 -0300

    Integração do BD Postgresql

--- servicos_a_porta/servicos_a_porta/settings.py
+++ servicos_a_porta/servicos_a_porta/settings.py
@@ -84,8 +84,11 @@ WSGI_APPLICATION = 'servicos_a_porta.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'servicosPorta',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': 'localhost'
     }
 }
 
