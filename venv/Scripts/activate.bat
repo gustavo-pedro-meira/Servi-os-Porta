7bf7b308b87c513e0c2eb4048871d0a60ca2be90
gustavo-pedro-meira <gustavomeira@ads.fiponline.edu.br>
2025-04-01 17:52:12 -0300

    Mesclagem Back e Front

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Gabriel\Documents\estudos\Desenvolvimento Web (UNIFIP - ADS)\Servi-os-Porta\Servi-os-Porta\venv
+set VIRTUAL_ENV=C:\Users\gusta\OneDrive\Documentos\Serviços à Porta\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
