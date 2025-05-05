654c66d167af0d05119476cdb82519a26a2390e6
gustavo-pedro-meira <gustavomeira@ads.fiponline.edu.br>
2025-05-05 15:29:33 -0300

    Validações no Serializers Post

--- servicos_a_porta/posts/api/serializers.py
+++ servicos_a_porta/posts/api/serializers.py
@@ -13,14 +13,34 @@ class ProfissionalSerializer(serializers.ModelSerializer):
 class PostSerializer(serializers.ModelSerializer):
     usuario = ProfissionalSerializer(source="profissional", read_only=True)
     profissional = serializers.PrimaryKeyRelatedField(queryset=Profissional.objects.all(), required=False)
+    curtidas_count = serializers.SerializerMethodField()
+    is_curtido = serializers.SerializerMethodField()
+
+    def get_curtidas_count(self, obj):
+        return obj.curtidas.count()
+    
+    def get_is_curtido(self, obj):
+        request = self.context.get("request").user
+        if user.is_authenticated:
+            return obj.curtidas.filter(usuario=user).exists()
+        return False
+    
+    def validate_conteudo(self, value):
+        if value:
+            if not value.name.lower().endswith(('.png', '.jpg', '.jpeg')):
+                raise serializers.ValidationError("Apenas imagens PNG ou JPEG são permitidas.")
+            if value.size > 5 * 1024 * 1024:
+                raise serializers.ValidationError("A imagem deve ter no máximo 5MB.")
+        return value
+
     class Meta:
         model = models.PostServico
-        fields = ["id", "titulo", "conteudo", "dataCriacao", "profissional", "curtidas", "usuario", "created_at", "updated_at", "is_active"]
-        read_only_fields = ["dataCriacao", "curtidas"]
-
+        fields = ["id", "titulo", "conteudo", "dataCriacao", "profissional", "idServico", "curtidas_count", "is_curtido", "usuario", "created_at", "updated_at", "is_active"]
+        read_only_fields = ["dataCriacao", "curtidas_count", "is_curtido", "usuario", "created_at", "updated_at", "is_active"]
         
 class ComentarioSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.ComentarioPost
         fields = "__all__"
         
+
